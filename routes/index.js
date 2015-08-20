309ea2cb692e539dc9d7650544b415e2b27e8946
César Arriaga <carriaga@users.noreply.github.com>
2015-08-20 21:28:48 +0200

    Moderación de comentarios y HTTPS

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,9 @@ router.get('/author', function(req, res) {
 	});
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load); // autoload: quizId
+router.param('commentId', commentController.load); // autoload: commentId
+
 
 // Definición de las rutas de session
 router.get('/login', sessionController.new);	// formulario login
@@ -26,13 +28,17 @@ router.get('/logout', sessionController.destroy);	// destruir sesión
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', quizController.new);
-router.post('/quizes/create', quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas con comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+		sessionController.loginRequired, commentController.publish);
+
 
 module.exports = router;
